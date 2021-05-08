48d2029b8adcc388390e1e39cc2ddf84e68488cf
Tatiana <tatigonzalez86@hotmail.com>
2021-05-07 22:41:24 -0400

    author Tatiana

---
+++ AccountTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+//*@author Tatiana
+
+class AccountTest<accountDescription> {
+    banking.Customer  cust;double initialBalance, String;accountDescription;
+
+
+
+}
+
+//Professor I tried to do this unit testing but had no idea what to do next...
